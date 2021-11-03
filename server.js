3a74f308af0797af95a8de06962ee4567522fc60
Devin Simpson <82175278+0xZ3n@users.noreply.github.com>
2021-11-02 21:50:09 -0400

    server issues and seeding

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-const sequelize = require('./config/connection');
+const sequelize = require("./config/connection");
 //const { sequelize } = require('./models/Product');
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
